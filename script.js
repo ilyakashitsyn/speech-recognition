2a6bbf7c2db54d77a213d0ffb4974ed113ca717d
Ilya Kashitsyn <ilyakashitsyn@gmail.com>
2025-05-06 22:11:11 -0300

    added writeMessage & checkNumber functions

--- script.js
+++ script.js
@@ -16,6 +16,38 @@ const onSpeak = e => {
   checkNumber(msg);
 };
 
+// Write what user speaks
+const writeMessage = msg => {
+  msgEl.innerHTML = `
+    <div>You said: </div>
+    <span class="box">${msg}</span>
+  `;
+};
+
+// Check msg against number
+const checkNumber = msg => {
+  const num = +msg;
+
+  // Check if valid number
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML = '<div>That is not a valid number</div>';
+    return;
+  }
+
+  // Check number
+  if (num === randomNum) {
+    document.body.innerHTML = `
+      <h2>Congrats! You have guessed the number! <br><br>
+      It was ${num}</h2>
+      <button class="play-again" id="play-again">Play Again</button>
+    `;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += '<div>GO LOWER</div>';
+  } else {
+    msgEl.innerHTML += '<div>GO HIGHER</div>';
+  }
+};
+
 // Generate random number
 const getRandomNumber = () => {
   return Math.floor(Math.random() * 100) + 1;
